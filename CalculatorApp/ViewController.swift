0091dcc9012030f3e70a4c1dbb31636fd255f9b0
Garrett Wolfe <gaw13@duke.edu>
2017-12-28 12:40:50 +1300

    Initial Commit

---
+++ CalculatorApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CalculatorApp
+//
+//  Created by Garrett Wolfe on 12/28/17.
+//  Copyright © 2017 Garrett Wolfe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
