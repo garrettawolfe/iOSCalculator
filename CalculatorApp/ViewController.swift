42cdd792902f71ff2c06a8c9f66f664ceb8c66f7
Garrett Wolfe <gaw13@duke.edu>
2017-12-28 21:46:24 +1300

    Completed basic functionality using stackview and constraints

--- CalculatorApp/ViewController.swift
+++ CalculatorApp/ViewController.swift
@@ -8,78 +8,105 @@
 
 import UIKit
 class ViewController: UIViewController {
-    var returnVal:Float = 0
-    var numOne:Float = 0
-    var numtwo:Float = 0
+    var returnVal:Float? = 0
+    var numOne:Float! = 0
+    var numtwo:Float! = 0
+    var op:String!=""
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        val.text="0"
         // Do any additional setup after loading the view, typically from a nib.
     }
 
     @IBAction func zero(_ sender: Any) {
-        val.text = "/(val.text)0"
+        val.text = (val.text!) + "0"
     }
     @IBAction func one(_ sender: Any) {
-        val.text = "/(val.text)1"
+        val.text = (val.text!) + "1"
     }
     @IBAction func two(_ sender: Any) {
-        val.text = "/(val.text)2"
+        val.text = (val.text!) + "2"
     }
     @IBAction func three(_ sender: Any) {
-        val.text = "/(val.text)3"
+        val.text = (val.text!) + "3"
     }
     @IBAction func four(_ sender: Any) {
-        val.text = "/(val.text)4"
+        val.text = (val.text!) + "4"
     }
     @IBAction func five(_ sender: Any) {
-        val.text = "/(val.text)5"
+        val.text = (val.text!) + "5"
     }
     @IBAction func six(_ sender: Any) {
-        val.text = "/(val.text)6"
+        val.text = (val.text!) + "6"
     }
     @IBAction func seven(_ sender: Any) {
-        val.text = "/(val.text)7"
+        val.text = (val.text!) + "7"
     }
     @IBAction func eight(_ sender: Any) {
-        val.text = "/(val.text)8"
+        val.text = (val.text!) + "8"
     }
     @IBAction func nine(_ sender: Any) {
-        val.text = "/(val.text)9"
+        val.text = (val.text!) + "9"
     }
     @IBAction func divide(_ sender: Any) {
-        numOne=(val.text) //convert to int
+        numOne=(Float)(val.text!) //convert to int
         val.text = ""
+        op="D"
     }
     @IBAction func multiply(_ sender: Any) {
-        numOne=(val.text) //convert to int
+        numOne=(Float)(val.text!) //convert to int
         val.text = ""
+        op="M"
 
     }
     @IBAction func add(_ sender: Any) {
-        numOne = (val.text)(Float) //convert to int
+        numOne=(Float)(val.text!) //convert to int
         val.text = ""
+        op="A"
 
     }
     @IBAction func subtract(_ sender: Any) {
-        numOne=(val.text) //convert to int
+        numOne=(Float)(val.text!) //convert to int
         val.text = ""
+        op="S"
 
     }
     @IBOutlet weak var val: UILabel!
+    //^this is the value displayed
+    
     @IBAction func clearVal(_ sender: UIButton) {
+        val.text=""
     }
     @IBAction func negate(_ sender: Any) {
+        //add a - to beginning
     }
     
     @IBAction func enter(_ sender: Any) {
-    numtwo = (val.text) //convert to int
-        returnVal = calcresult(num1:numOne, num2:numtwo)
+    numtwo=Float(val.text!)
+        returnVal = calcresult(num1:numOne, num2:numtwo, op:op)
+        val.text = returnVal!.description
     }
     
     @IBAction func mod(_ sender: Any) {
+        var n = (Float)(val.text!)
+        n=n!/100
+        val.text = n!.description
     }
-    func calcresult(num1:Float, num2:Float)-> Float{
+    func calcresult(num1:Float, num2:Float, op:String)-> Float{
     //fill in using nums
+        switch op{
+        case "A":
+            return (num1+num2)
+        case "M":
+            return (num1*num2)
+        case "D":
+            return (num1/num2)
+        case "S":
+            return (num1-num2)
+        default:
+            return 0
+        }
     }
     
     
